171bde842f6a3ebf4acaded687d79ab0e25ebc05
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2021-05-05 13:24:58 +0000

    Hopefully latest next doesn't need this hack

--- next.config.js
+++ next.config.js
@@ -6,12 +6,7 @@ module.exports = withTM({
   future: {
     webpack5: true,
   },
-  webpack: (config, { isServer, dev }) => {
-    // https://github.com/vercel/next.js/issues/22813#issuecomment-810961712
-    config.output.chunkFilename = isServer
-      ? `${dev ? "[name]" : "[name].[fullhash]"}.js`
-      : `static/chunks/${dev ? "[name]" : "[name].[fullhash]"}.js`;
-
+  webpack: (config, { isServer }) => {
     // https://github.com/vercel/next.js/issues/19865#issuecomment-810738415
     config.output.hotUpdateMainFilename =
       "static/webpack/[fullhash].[runtime].hot-update.json";
