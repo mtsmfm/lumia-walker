d355dfcf5d1319aa8a1451b878220499ef4279f1
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2021-05-05 13:24:00 +0000

    Fix hmr

--- next.config.js
+++ next.config.js
@@ -6,11 +6,19 @@ module.exports = withTM({
   future: {
     webpack5: true,
   },
-  // https://github.com/vercel/next.js/issues/22813#issuecomment-810961712
   webpack: (config, { isServer, dev }) => {
+    // https://github.com/vercel/next.js/issues/22813#issuecomment-810961712
     config.output.chunkFilename = isServer
       ? `${dev ? "[name]" : "[name].[fullhash]"}.js`
       : `static/chunks/${dev ? "[name]" : "[name].[fullhash]"}.js`;
+
+    // https://github.com/vercel/next.js/issues/19865#issuecomment-810738415
+    config.output.hotUpdateMainFilename =
+      "static/webpack/[fullhash].[runtime].hot-update.json";
+
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
 
     return config;
   },
